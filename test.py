1de93b89e340b91ca196a24ad67611c8e1311067
mariiamorina17 <mariiamorina17@mail.ru>
2024-11-06 16:24:44 +0300

    doc

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pytest
 from pop3main import read_pop3_emails
 
 def test_read_pop3_emails():
-    assert read_pop3_emails('pop.mail.ru', 'mariiamorina@yandex.ru', 'usoxwngsgsxurgsc') == None
+    assert read_pop3_emails('pop.yandex.ru', 'mariiamorina@yandex.ru', 'usoxwngsgsxurgsc') == None
 
 def test_read_pop3_emails_mailru():
     with pytest.raises(TypeError):
